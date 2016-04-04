333ab1409d61101333f4c336a61d10ccb3716e4b
Jeffrey D <literallyelvis@gmail.com>
2016-04-03 21:50:15 -0500

    Unexport both methods, some golint concerns addressed.

--- captain.go
+++ captain.go
@@ -16,19 +16,19 @@ import (
 // by Github's Webhooks API: https://developer.github.com/webhooks/
 
 // Note that this is designed for Github specifically.
-func WebhookIsAuthenticated(xHubSignature string, hookBody []byte) bool {
-	hmac_digest := hmac.New(sha1.New, []byte(os.Getenv("GITHOOK_SECRET")))
-	hmac_digest.Write(hookBody)
+func webhookIsAuthenticated(xHubSignature string, hookBody []byte) bool {
+	hmacDigest := hmac.New(sha1.New, []byte(os.Getenv("GITHOOK_SECRET")))
+	hmacDigest.Write(hookBody)
 
 	var buffer bytes.Buffer
 	// all signatures are preceeded with 'sha1='
 	buffer.WriteString("sha1=")
-	buffer.WriteString(hex.EncodeToString(hmac_digest.Sum(nil)))
+	buffer.WriteString(hex.EncodeToString(hmacDigest.Sum(nil)))
 
 	return hmac.Equal(buffer.Bytes(), []byte(xHubSignature))
 }
 
-func GithookListener(w http.ResponseWriter, req *http.Request) {
+func githookListener(w http.ResponseWriter, req *http.Request) {
 	// all Github-sent webhooks are POST requests, we should reject otherwise.
 	if req.Method == "POST" {
 		body, err := ioutil.ReadAll(req.Body)
@@ -38,7 +38,7 @@ func GithookListener(w http.ResponseWriter, req *http.Request) {
 
 		// if no header is present for X-Hub-Signature, we should reject the request.
 		if len(req.Header["X-Hub-Signature"]) > 0 {
-			if WebhookIsAuthenticated(req.Header["X-Hub-Signature"][0], body) {
+			if webhookIsAuthenticated(req.Header["X-Hub-Signature"][0], body) {
 				fmt.Println("Received Webhook is authentic")
 				// do whatever here
 			} else {
@@ -54,7 +54,7 @@ func GithookListener(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	// sets up a simple route for your webhook to send payloads to
-	http.HandleFunc("/build/github", GithookListener)
+	http.HandleFunc("/build/github", githookListener)
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
